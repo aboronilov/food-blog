469dbb40a2c53d4d7bcb7d255b6ef5d66ad45172
anatoly <boronilov@gmail.com>
2022-09-26 17:04:28 +0200

    serializers, views

--- backend/blog_api/admin.py
+++ backend/blog_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Category, Recipe
+
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Recipe)
--- backend/blog_api/views.py
+++ backend/blog_api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Category, Recipe
+from .serializers import CategorySerializer, RecipeSerializer
+from rest_framework import viewsets, mixins
+from rest_framework.response import Response
 
-# Create your views here.
+
+class RecipeApiView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+    lookup_field = "slug"
+
+
+class CategoryApiView(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.RetrieveModelMixin):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryRecipeViewSet(viewsets.ViewSet):
+    def retrieve(self, request, *args, **kwargs):
+        queryset = Recipe.objects.none()
+        pk = kwargs.get("pk")
+        if pk is not None:
+            queryset = Recipe.objects.filter(category=pk)
+        serializer = RecipeSerializer(queryset, many=True)
+        return Response(serializer.data)
